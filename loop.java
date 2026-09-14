da5a54d9f7b6e330c3bb90f6d1c9d24f5de46221
Collins Mucho <72305974+sleepchild@users.noreply.github.com>
2026-09-14 08:13:14 +0200

    gh

---
+++ loop.java
@@ -0,0 +1,17 @@
+/**
+ * @Author sldge
+ * @Date 2026/09/14 08:12
+ */
+public class loop {
+    
+    public static void execute(){
+        
+        while(true){
+            
+            try {
+                Thread.sleep(100000);
+            } catch (InterruptedException e) {}
+        }
+    }
+    
+}
